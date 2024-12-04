9976484a86f14510395114d7498da391302817bb
Aïolah Vaïti <yoyo31.music@gmail.com>
2024-12-04 09:08:12 +0100

    Ajout d'un commentaire

--- src/main/java/org/github/vaiti/Main.java
+++ src/main/java/org/github/vaiti/Main.java
@@ -1,7 +1,10 @@
 package org.github.vaiti;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // This is a comment
         System.out.println("Hello world!");
     }
 }
